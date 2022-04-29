f9aad438a41ac9c9dc1e8ce13e77380755c26aff
Леонов Михаил <mishaleonov00@mail.ru>
2022-04-29 18:38:27 +0300

    Add files via upload
    bootable version

---
+++ generalization/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows;
+
+namespace generalization
+{
+    class Class1<T> //обобщенный класс
+    {
+        delegate int Square(int x);
+        T[] data; //Массив с данными о количестве
+        public Class1()
+        {
+            data = new T[0];
+
+        }
+        public void Add(T item) // метод добавления в массив
+        {
+            T[] newData = new T[data.Length + 1];
+            for (int i = 0; i < data.Length; i++)
+            {
+                newData[i] = data[i];
+                
+            }
+
+            newData[data.Length] = item;
+            data = newData;
+        }
+
+        public void Resize(T item) // метод удаления из массива
+        {
+            T[] newData = new T[data.Length + 1];
+            for (int i = 0; i > data.Length; i--)
+            {
+                newData[i] = data[i];
+            }
+
+            newData[data.Length] = item;
+            data = newData;
+        }
+
+        public void Square1(T item)
+        {
+            T[] newData = new T[data.Length + 1];
+            for (int i = 0; i < data.Length; i++)
+            {
+                newData[i] = data[i];
+
+            }
+
+            newData[data.Length] = item;
+            data = newData;
+        }
+    }
+
+
+
+
+    //class Message
+    //{
+    //    delegate void Message1();
+
+    //    static void Main(string[] args)
+    //    {
+    //        Message1 mes = Specifications;
+    //        mes();
+    //    }
+
+    //    public static void Specifications()
+    //    {
+    //        MessageBox.Show(File.ReadAllText(@"C:\Users\misha\source\repos\generalization\generalization\resourse\Boeing747-400.txt"));
+    //    }
+    //}
+}
---
+++ generalization/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using static System.Math;
+
+namespace generalization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int i = 0;
+        delegate void Message(); // объявление делегата для вывода Характеристик
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // присваивание адреса метода через контруктор
+            Message mes1 = Specifications; // делегат указывает на метод Specifications, который находится ниже
+            mes1(); // Вызов
+
+            static void Specifications()
+            {
+                MessageBox.Show(File.ReadAllText(@"C:\Users\misha\source\repos\generalization\generalization\resourse\Boeing747-400.txt"));
+            }
+
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Class1<int> number = new Class1<int>();
+
+            number.Add(i++);
+
+            LabelValue.Content = "Колличество на складе: " + i + " шт";
+        }
+
+        private void ButtonResize_Click(object sender, RoutedEventArgs e)
+        {
+            Class1<int> number = new Class1<int>();
+
+            if (i > 0)
+            {
+                number.Resize(i--);
+
+                LabelValue.Content = "Колличество на складе: " + i + " шт";
+            }
+            else
+            {
+                MessageBox.Show("Колличество не может быть отрицательным");
+            }
+        }
+    }
+}
